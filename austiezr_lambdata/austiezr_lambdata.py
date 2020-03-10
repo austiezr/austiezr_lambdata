13ac44a42f98170b530889504c54ee60a2b1814b
austiezr <austinziech@gmail.com>
2020-03-10 17:43:37 -0600

    namespacing fixes

--- austiezr_lambdata/austiezr_lambdata.py
+++ austiezr_lambdata/austiezr_lambdata.py
@@ -8,7 +8,7 @@ from sklearn.metrics import mean_absolute_error as mae
 
 
 class TransformDF:
-    """The base class for all df transformations"""
+    """base class for all df transformations"""
 
     @staticmethod
     def date_split(df, date_col):
@@ -37,7 +37,7 @@ class MVP:
     def __init__(self, model):
         self.model = model
 
-    def fastFirst(self, df, target):
+    def fast_first(self, df, target):
         """
         Quick method for producing baseline results
         returns baseline, splits data, fits basic encoder/imputer/model
@@ -47,10 +47,10 @@ class MVP:
 
         train, test = train_test_split(df, train_size=0.80, test_size=0.20, random_state=33)
 
-        X_train = train.drop(columns=[target])
+        x_train = train.drop(columns=[target])
         y_train = train[target]
 
-        X_test = test.drop(columns=[target])
+        x_test = test.drop(columns=[target])
         y_test = test[target]
 
         pipe = make_pipeline(
@@ -59,7 +59,7 @@ class MVP:
             self.model
         )
 
-        pipe.fit(X=X_train, y=y_train)
+        pipe.fit(X=x_train, y=y_train)
 
         try:
             mean = round(np.mean(df[target]))
@@ -67,9 +67,9 @@ class MVP:
             y_pred[target] = mean
             y_pred = y_pred[target]
             print(f'Baseline MAE: {mae(df[target], y_pred)}\n')
-            print(f'Training MAE: {mae(y_train, pipe.predict(X_train))}\n')
-            print(f'Test MAE: {mae(y_test, pipe.predict(X_test))}\n')
+            print(f'Training MAE: {mae(y_train, pipe.predict(x_train))}\n')
+            print(f'Test MAE: {mae(y_test, pipe.predict(x_test))}\n')
         except TypeError:
             print(f'Baseline Accuracy:\n{df[target].value_counts(normalize=True)}\n')
-            print(f'\nTraining Accuracy: {pipe.score(X_train, y_train)}\n')
-            print(f'Test Accuracy: {pipe.score(X_test, y_test)}\n')
+            print(f'\nTraining Accuracy: {pipe.score(x_train, y_train)}\n')
+            print(f'Test Accuracy: {pipe.score(x_test, y_test)}\n')
