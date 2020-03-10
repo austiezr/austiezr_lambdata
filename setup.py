967877b6388f957d61d0c2a6d42865027efde1cc
austiezr <austinziech@gmail.com>
2020-03-10 17:51:56 -0600

    namespacing fixes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="austiezr_lambdata", # Replace with your own username
-    version="0.2.4",
+    version="0.2.5",
     author="Example Author",
     author_email="author@example.com",
     description="A small example package",
